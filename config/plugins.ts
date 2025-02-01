98b26447616c9fb6dc842ee11bf08c88109e639d
JanteWinkel <janpaultewinkelj@gmail.com>
2025-02-01 16:04:26 -0400

    feat: cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,16 @@
-
-// module.exports = {
-//    'strapi-neon-tech-db-branches': {
-//      enabled: true,
-//      config: {
-//        neonApiKey: "2bl16pcox5i4rex7b0ysfmft4suirwv8hp3a2gae6jdh52vw24ikf5x76sink4sn", // get it from here: https://console.neon.tech/app/settings/api-keys
-//        neonProjectName: "ecommerce-sochi", // the neon project under wich your DB runs
-//        neonRole: "neondb_owner", // create it manually under roles for your project first
-//        gitBranch: "main" //branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
-//      },
-//    },
-//  };
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
